d1052f87710f60c70f1affb60c9b012f8f6a4f29
Robert Brem <robert.brem@isceco.admin.ch>
2018-09-04 15:29:33 +0200

    [REFAC] use german mirrors

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+sed -i -e 's~ch.archive.ubuntu.com/ubuntu/~de.archive.ubuntu.com/ubuntu/~' /etc/apt/sources.list
+
 apt-add-repository ppa:ansible/ansible -y
 apt-get update
 apt-get install ansible -y
--- startAnsible.sh
+++ startAnsible.sh
@@ -3,7 +3,7 @@
 cd /home/vagrant
 mkdir projects
 cd projects
-git clone https://${USERNAME}:${PASSWORD}@${REPOSITORY} setup
+git clone https://${GIT_USERNAME}:${GIT_PASSWORD}@${REPOSITORY} setup
 cd setup
 ansible-galaxy install -r requirements.yml
-ansible-playbook playbook.yml 
+ansible-playbook playbook.yml
